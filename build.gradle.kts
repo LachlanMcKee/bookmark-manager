245af847a434696f563c9a870c95082c4d016e04
Lachlan McKee <lachlandevelopment@gmail.com>
2020-12-19 19:06:44 +0000

    Upgraded to AS Arctic Fox alpha3

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     jcenter()
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:4.2.0-alpha16")
+    classpath("com.android.tools.build:gradle:7.0.0-alpha03")
     classpath(kotlin("gradle-plugin", version = Dependencies.Kotlin.version))
 
     classpath("com.google.gms:google-services:4.3.4")
