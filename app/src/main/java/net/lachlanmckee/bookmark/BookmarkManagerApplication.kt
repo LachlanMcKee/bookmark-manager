95f92f1ca50d311773ac1c6ee42464a1b792bb69
Lachlan McKee <lachlandevelopment@gmail.com>
2021-04-11 14:43:51 +0100

    Updated Gradle to 7.0 and updated all relevant dependencies

--- app/src/main/java/net/lachlanmckee/bookmark/BookmarkManagerApplication.kt
+++ app/src/main/java/net/lachlanmckee/bookmark/BookmarkManagerApplication.kt
@@ -5,7 +5,7 @@ import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class LinkCleanerApplication : Application() {
+class BookmarkManagerApplication : Application() {
   override fun onCreate() {
     super.onCreate()
     Timber.plant(Timber.DebugTree())
