9b0ae75cbc0ef87291df95bb29bf9997503fcca5
Lachlan McKee <lachlandevelopment@gmail.com>
2022-03-26 22:08:24 +0000

    Improved scrolling performance of home screen

--- features/bookmark-form/src/main/java/net/lachlanmckee/bookmark/feature/form/BookmarkFormViewModel.kt
+++ features/bookmark-form/src/main/java/net/lachlanmckee/bookmark/feature/form/BookmarkFormViewModel.kt
@@ -1,11 +1,13 @@
 package net.lachlanmckee.bookmark.feature.form
 
+import androidx.compose.runtime.Immutable
 import net.lachlanmckee.bookmark.feature.BookmarkViewModel
 import net.lachlanmckee.bookmark.feature.form.BookmarkFormViewModel.Event
 import net.lachlanmckee.bookmark.feature.form.BookmarkFormViewModel.State
 
 internal interface BookmarkFormViewModel : BookmarkViewModel<State, Event> {
 
+  @Immutable
   data class State(
     val name: String,
     val url: String,
